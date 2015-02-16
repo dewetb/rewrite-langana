3f86d25c2c4dd9039837171be6414356f38b403f
De Wet Blomerus <dewet@blomerus.org>
2015-02-16 20:36:55 +0200

    name required to sign up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,11 @@ describe User do
     expect(@user.email).to match 'user@example.com'
   end
 
+  it "requires a name" do
+    user = User.new(name: "")
+    
+    expect(user.valid?).to eq(false)
+    expect(user.errors[:name].any?).to eq(true)
+  end
+
 end
