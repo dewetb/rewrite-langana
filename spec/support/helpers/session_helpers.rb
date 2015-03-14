eda2c76ae2d1dc3583ad14951da0aff48b4e60e0
De Wet Blomerus <dewet@blomerus.org>
2015-03-14 22:45:53 +0200

    spec select radio button

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,7 +2,7 @@ module Features
   module SessionHelpers
     def sign_up_with(role, firstname, lastname, email, password, confirmation)
       visit new_user_registration_path
-      select 'Looking for work'
+      choose 'user_role_worker'
       fill_in 'user_firstname', with: firstname
       fill_in 'user_lastname', with: lastname
       fill_in 'Email', with: email
