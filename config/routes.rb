0a48709e5907b2164cd2c836d5b3db2c15e3a064
De Wet Blomerus <dewet@blomerus.org>
2015-03-16 21:52:18 +0200

    simplify user edit role

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-
-  devise_for :users, controllers: { registrations: "users/registrations" }
-
+  devise_for :users
   resources :users
 end
