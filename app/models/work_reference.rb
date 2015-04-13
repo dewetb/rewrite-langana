0bc18d8c5adedb3bd286d0b6fa26d2ad7315bb1d
De Wet Blomerus <dewet@blomerus.org>
2015-04-13 22:00:49 +0200

    pass tests for work reference

--- app/models/work_reference.rb
+++ app/models/work_reference.rb
@@ -1,4 +1,8 @@
 class WorkReference < ActiveRecord::Base
   belongs_to :employer_user, class_name: "User"
   belongs_to :worker_user, class_name: "User"
+
+  validates :work, presence: true
+  validates :comment, presence: true
+
 end
