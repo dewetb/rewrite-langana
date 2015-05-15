9e65b78a9a345a88a26751e3e129a3f5c20f0159
De Wet Blomerus <dewet@blomerus.org>
2015-05-15 22:36:06 +0200

    upload to s3 with carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]                         # required
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_acl    = :public_read
+  config.asset_host = 'http://example.com'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            's3-eu-west-1'
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
