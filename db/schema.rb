48a91eceab3ef829b3f7b801cff92fdbd6e3b3fb
De Wet Blomerus <dewet@blomerus.org>
2015-05-28 16:33:23 +0200

    index users on phone, because this is the most important app ever

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150528103941) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["phone"], name: "index_users_on_phone", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "work_references", force: :cascade do |t|
